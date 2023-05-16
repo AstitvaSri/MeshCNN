19c27980341c93d24c4c4dc0e3256e92c2b66cff
vivekmadhavaram <31122207+vivekmadhavaram@users.noreply.github.com>
2023-05-16 13:03:26 +0530

    Update mesh_classifier.py

--- models/mesh_classifier.py
+++ models/mesh_classifier.py
@@ -75,7 +75,7 @@ class ClassifierModel:
         net = self.net
         if isinstance(net, torch.nn.DataParallel):
             net = net.module
-        print('loading the model from /content/drive/MyDrive/3DVSS_Graph/MeshCNN/latest_net.pth)
+        print('loading the model from /content/drive/MyDrive/3DVSS_Graph/MeshCNN/latest_net.pth')
         # PyTorch newer than 0.4 (e.g., built from
         # GitHub source), you can remove str() on self.device
         state_dict = torch.load("/content/drive/MyDrive/3DVSS_Graph/MeshCNN/latest_net.pth", map_location=str(self.device))
